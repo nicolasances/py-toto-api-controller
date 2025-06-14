ef0a91481bfcf02539029e87c33867b9d85afa24
nicolasances <nicolas.matteazzi@gmail.com>
2025-06-14 15:24:16 +0200

    v 1.1.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='totoapicontroller',
-    version='1.1.4',
+    version='1.1.5',
     description='API Controller for Toto APIs',
     author='nicolasances',
     author_email='nicolas.matteazzi@gmail.com',
